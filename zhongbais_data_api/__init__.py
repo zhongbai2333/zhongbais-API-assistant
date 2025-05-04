e2c31e20d892aad052da43ee1e6f4204a1354369
zhong_bai <54356787+zhongbai2333@users.noreply.github.com>
2025-05-05 01:55:11 +0800

    Create: Add zhongbaisDataAPI Plugin

---
+++ zhongbais_data_api/__init__.py
@@ -0,0 +1,18 @@
+def get_player_info() -> dict:
+    """
+    获取玩家信息字典
+
+    Returns:
+        dict: 玩家信息
+    """
+    pass
+
+
+def get_player_list() -> list:
+    """
+    获取在线玩家列表
+
+    Returns:
+        list: 玩家列表
+    """
+    pass
