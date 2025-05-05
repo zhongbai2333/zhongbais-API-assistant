b76dbdca373f1621fb23fc74a67790764ab3495f
zhong_bai <54356787+zhongbai2333@users.noreply.github.com>
2025-05-05 10:29:47 +0800

    Update: dataAPI

--- zhongbais_data_api/__init__.py
+++ zhongbais_data_api/__init__.py
@@ -1,18 +1,44 @@
-def get_player_info() -> dict:
-    """
-    获取玩家信息字典
+class zbDataAPI(object):
+    @staticmethod
+    def get_player_info() -> dict:
+        """
+        获取玩家信息字典
 
-    Returns:
-        dict: 玩家信息
-    """
-    pass
+        Returns:
+            dict: 玩家信息
+        """
+        pass
 
+    @staticmethod
+    def register_player_info_callback(func: function) -> dict:
+        """
+        注册回调函数，在player_info发生变化的时候触发
 
-def get_player_list() -> list:
-    """
-    获取在线玩家列表
+        Args:
+            func (function): 回调函数接口
+        Returns:
+            dict: 玩家信息
+        """
+        pass
 
-    Returns:
-        list: 玩家列表
-    """
-    pass
+    @staticmethod
+    def get_player_list() -> list:
+        """
+        获取在线玩家列表
+
+        Returns:
+            list: 玩家列表
+        """
+        pass
+
+    @staticmethod
+    def register_player_list_callback(func: function) -> list:
+        """
+        注册回调函数，在player_list发生变化的时候触发
+
+        Args:
+            func (function): 回调函数接口
+        Returns:
+            list: 玩家列表
+        """
+        pass
